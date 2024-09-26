e02c38b1d6df263e1646144d3c6d81fdaf7d58bc
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-09-26 16:04:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// String de entrada
+const input = "Chinelo";
+
+function verifyString(string) {
+  if (typeof string !== "string") {
+    console.log("Informe apenas valores que sejam string")
+  }
+
+  let quantity = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === "a") {
+      quantity++;
+    }
+  }
+
+  if (quantity === 0) {
+    console.log(`A palavra ${string} não possui letra a`);
+  } else {
+    console.log(`A palavra ${string} possui ${letraA} letras a`);
+  }
+
+}
+
+verifyString(input);
